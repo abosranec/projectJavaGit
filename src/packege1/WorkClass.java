4b66d7adfbb4d8c8e8366b052a3553587419164d
pasha <pasha_mugako@mail.ru>
2016-03-11 22:49:25 +0300

    Initial commit

---
+++ src/packege1/WorkClass.java
@@ -0,0 +1,10 @@
+package packege1;
+
+/**
+ * Created by Pasha on 11.03.2016.
+ */
+public class WorkClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
