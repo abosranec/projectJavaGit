47fb07767916bb3ff3ecf4ceb7c8e81692ccd520
pasha <pasha_mugako@mail.ru>
2016-03-11 23:31:03 +0300

    add 2 commit

--- src/packege1/WorkClass.java
+++ src/packege1/WorkClass.java
@@ -7,5 +7,6 @@ public class WorkClass {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git add 1 commit");
+        System.out.println("Hello Git add 2 commit");
     }
 }
